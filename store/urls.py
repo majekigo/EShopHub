cc481cc6b7528830f275307260382025e1b35279
Zakhar <majekigo@gmail.com>
2023-12-15 02:07:14 +0300

    Обновление 1.4 - Сделаны CreateView, ListView, UpdateView, DeleteView, DetailView - Обновлены urls.py - Обновлена база данных - Удалена фотография - Изменен catalog.html

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,27 @@
 from django.urls import path
+from django.conf.urls.static import static
+from EShopHub import settings
 from store import views
+from store.views import ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, CategoryListView, \
+    CategoryDetailView, CategoryCreateView, TagListView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('catalog/', views.catalog, name='catalog')
+    path('catalog/', views.catalog, name='catalog'),
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='view_product'),
+    path('catalog/add/', ProductCreateView.as_view(), name='product_add'),
+    path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+    path('products_by_category/<int:category_id>/', views.products_by_category, name='products_by_category'),
+    path('products_by_tag/<str:tag_name>/', views.products_by_tag, name='products_by_tag'),
+    path('catalog/categories/', CategoryListView.as_view(), name='category_list'),
+    path('catalog/categories/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('catalog/categories/add/', CategoryCreateView.as_view(), name='category_add'),
+    path('catalog/tags/', TagListView.as_view(), name='tag_list'),
+    path('catalog/tags/<int:pk>/', views.TagDetailView.as_view(), name='tag_detail'),
+    path('catalog/tags/add/', views.TagCreateView.as_view(), name='tag_add'),
+    path('catalog/add_category/', views.add_category, name='add_category'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,11 +1,127 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
+from store.forms import CategoryForm, ProductForm, TagForm
+from store.models import Product, Tag, Category
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 
-
+#Начало функций
 def index(request):
     return render(request, template_name='index.html')
 
 
 def catalog(request):
-    return render(request, template_name='catalog.html')
+    product_list = Product.objects.all()
+    paginator = Paginator(product_list, 1)
+
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    return render(request, 'catalog.html', {'products': products})
+
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm()
+    return render(request, 'add_category.html', {'form': form})
+
+
+def products_by_tag(request, tag_name):
+    tag = get_object_or_404(Tag, name=tag_name)
+    products_with_tag = Product.objects.filter(tags=tag)
+    return render(request, 'products_by_tag.html', {'products': products_with_tag, 'tag': tag})
+
+
+def products_by_category(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    products_in_category = Product.objects.filter(categories=category)
+    return render(request, 'products_by_category.html', {'products': products_in_category, 'category': category})
+
+
+#Product
+class ProductListView(ListView):
+    model = Product
+    template_name = 'ProductView/product_list.html'
+    context_object_name = 'products'
+    paginate_by = 2
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'ProductView/product_about.html'
+    context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'ProductView/product_add.html'
+    success_url = reverse_lazy('catalog')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'ProductView/product_update.html'
+    context_object_name = 'product'
+    success_url = reverse_lazy('catalog')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'ProductView/product_delete.html'
+    context_object_name = 'product'
+    success_url = reverse_lazy('catalog')
+
+
+#Category
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'CategoryView/category_list.html'
+    context_object_name = 'categories'
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'CategoryView/category_detail.html'
+    context_object_name = 'category'
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'CategoryView/category_add.html'
+    success_url = reverse_lazy('category_list')
+
+
+#Tag
+class TagListView(ListView):
+    model = Tag
+    template_name = 'TagView/tag_list.html'
+    context_object_name = 'tags'
+
+
+class TagDetailView(DetailView):
+    model = Tag
+    template_name = 'TagView/tag_detail.html'
+    context_object_name = 'tag'
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    form_class = TagForm
+    template_name = 'TagView/tag_add.html'
+    success_url = reverse_lazy('tag_list')
