dcd2d7f259b761a9c6801f67073db69a1b494e1f
Zakhar <majekigo@gmail.com>
2023-12-15 00:56:53 +0300

    Обновление 1.3 - Изменения в файлах admin.py и models.py - Обновлена база данных

--- store/models.py
+++ store/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='catalog/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE, default=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    order_number = models.AutoField(primary_key=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivery_address = models.TextField()
+    customer_phone = models.CharField(max_length=20)
+    customer_name = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product, through='OrderPosition')
+
+    def __str__(self):
+        return f"Order {self.order_number}"
+
+
+class OrderPosition(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.order_number}"
