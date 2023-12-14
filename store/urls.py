c7da2dd6c9ef7904fb3e6283046b7d79ef86e2de
Zakhar <majekigo@gmail.com>
2023-12-15 00:41:03 +0300

    Обновление 1.2 - Добавлена страница catalog.html

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from store import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('catalog/', views.catalog, name='catalog')
 ]
--- store/views.py
+++ store/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, template_name='index.html')
+
+
+def catalog(request):
+    return render(request, template_name='catalog.html')
